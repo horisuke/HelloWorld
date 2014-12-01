1414aad69281ba0ec5ed4e1f0466377bee26435a
nlf <nlf@nlf.com>
2014-12-02 01:17:46 +0900

    clone, modify and push in SubPC.

--- cpp/helloworld.cpp
+++ cpp/helloworld.cpp
@@ -5,6 +5,7 @@ int main()
 	std::cout << "Hello, World!" << std::endl;
 	std::cout << "Hello, GitHub!" << std::endl;
 	std::cout << "make cpp directory" << std::endl;
+	std::cout << "push from SubPC." << std::endl;
 	return 0;
 }
 
