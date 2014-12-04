6d616ae918b26ab8abe21383e284ebd6becb1b16
hori3 <horisuke@gmai.com>
2014-12-04 21:43:41 +0900

    Add new python file for httpcommunication in proxy environment.

---
+++ python/HelloPython/src/httpcommunication_proxy.py
@@ -0,0 +1,17 @@
+#python2.7
+#import urllib
+#
+#proxies = {'http': 'http://proxy.server:port'}
+#url = 'http://www.yahoo.co.jp'
+#filehandle = urllib.urlopen(url, proxies=proxies)
+#data = filehandle.read()
+#print(data)
+
+#python3.4
+import urllib.request
+proxies = 'http://proxy.server:port'
+url = 'http://www.yahoo.co.jp'
+request = urllib.request.Request(url)
+request.set_proxy(proxies, 'http')
+response = urllib.request.urlopen(request)
+print(response.read().decode('utf-8'))
