7e985d18939ee59cbe831781f42539cf457eff3b
hori3 <horisuke@gmai.com>
2014-12-12 20:21:08 +0900

    Add and Modify the Linux command to be executed.

--- python/HelloPython/src/executeLinuxCommandbyPython.py
+++ python/HelloPython/src/executeLinuxCommandbyPython.py
@@ -1,11 +1,17 @@
 import subprocess
 
 #cmd = "ls | head -n 5"
+cmd0 = "pwd"
 cmd = "ls -lt"
 #cmd = "head -n 5 sample.py"
+subprocess.call(cmd0, shell=True)
 subprocess.call(cmd, shell=True)
+
 print()
 print("*****")
+print()
 
+ret0 = subprocess.check_output(cmd0.split(" ")).decode('utf-8')
 ret = subprocess.check_output(cmd.split(" ")).decode('utf-8')
+print(ret0)
 print(ret)
